8ff5a36c21f0bff0c3ae6d4b2fcdb5409b6989ed
Nicolai <nicolaigaina@gmail.com>
2017-09-25 12:45:37 -0700

    introduced mixins and gulp error handling.

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,12 +3,16 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssimport = require('postcss-import');
+cssimport = require('postcss-import'),
+mixins = require('postcss-mixins');
 
-gulp.task('styles', (done) =>  {
+gulp.task('styles', () =>  {
     return gulp.src('./app/assets/styles/styles.css')
-                 .pipe(postcss([cssimport, cssvars, nested, autoprefixer]))
-                 .on('error', done)
+                 .pipe(postcss([cssimport,mixins, cssvars, nested, autoprefixer]))
+                 .on('error', function(error) {
+                     console.log(error.toString());
+                     this.emit('end');
+                 })
                  .pipe(gulp.dest('./app/public/styles'));
                  
 });
