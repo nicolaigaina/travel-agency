2b6aaec332384be8abfaac5396bcc7064392f7e5
Nicolai <nicolaigaina@gmail.com>
2017-09-24 17:49:15 -0700

    added global styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,31 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested');
 
 gulp.task('default', () => {
     console.log('Default task is running....');
 });
+
+gulp.task('html', () => {
+    console.log('Html task is running...');
+});
+
+gulp.task('styles', () => {
+   return gulp.src('./app/assets/styles/styles.css')
+                .pipe(postcss([cssvars, nested, autoprefixer]))
+                .pipe(gulp.dest('./app/public/styles'));
+});
+
+gulp.task('watch', () => {
+    watch('./app/index.html', () => {
+        gulp.start('html');
+    });
+
+    watch('./app/assets/styles/**/*.css', () => {
+        gulp.start('styles');
+    });
+});
+
