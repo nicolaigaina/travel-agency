958aeca1b803bb91a961afb5021763216b664dc0
Nicolai <nicolaigaina@gmail.com>
2017-09-28 16:03:32 -0700

    Completed automatic sprite generation.

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,8 +1,12 @@
 var gulp = require('gulp'),
-svgSprite = require('gulp-svg-sprite');
+svgSprite = require('gulp-svg-sprite'),
+rename = require('gulp-rename'),
+del = require('del');
+
 var config = {
     mode: {
         css: {
+            sprite: 'sprite.svg',
             render: {
                 css: {
                     template: './gulp/templates/sprite.css'
@@ -12,8 +16,29 @@ var config = {
     }
 };
 
-gulp.task('createSprite', () => {
+gulp.task('beginClean', () => {
+    return del(['./app/public/sprite', './app/assets/images/sprite']);
+});
+
+gulp.task('createSprite', ['beginClean'], () => {
     return gulp.src('./app/assets/images/icons/**/*.svg')
         .pipe(svgSprite(config))
         .pipe(gulp.dest('./app/public/sprite/'));
 });
+
+gulp.task('copySpriteGraphic', ['createSprite'], () => {
+    return gulp.src('./app/public/sprite/css/**/*.svg')
+        .pipe(gulp.dest('./app/assets/images/sprites'));
+});
+
+gulp.task('copySpriteCSS',['createSprite'], () => {
+    return gulp.src('./app/public/sprite/css/*.css')
+        .pipe(rename('_sprite.css'))
+        .pipe(gulp.dest('./app/assets/styles/components'));
+});
+
+gulp.task('endClean',['copySpriteGraphic', 'copySpriteCSS'], () => {
+    return del('./app/public/sprite');
+});
+
+gulp.task('icons', ['beginClean','createSprite','copySpriteGraphic', 'copySpriteCSS', 'endClean']);
